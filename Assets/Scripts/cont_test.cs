53016bb4590f00f28b37362116a89b0e6c5118b3
伊藤裕希 <51065730+mynet-yukito@users.noreply.github.com>
2020-04-05 03:19:10 +0900

    コントローラー設定

---
+++ Assets/Scripts/cont_test.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cont_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float hori = Input.GetAxis ("X axis");
+		float vert = Input.GetAxis ("Y axis");
+		if(( hori != 0) ||  (vert != 0) ){
+			Debug.Log ("stick:"+hori+","+vert );
+		}
+	}
+
+	void OnGUI () {
+		GUILayout.Label(AxisLabel("X axis"));
+		GUILayout.Label(AxisLabel("Y axis"));
+		GUILayout.Label(AxisLabel("3rd axis"));
+		GUILayout.Label(AxisLabel("6th axis"));
+		GUILayout.Label(AxisLabel("7th axis"));
+		GUILayout.Label(AxisLabel("8th axis"));
+	
+		GUILayout.Label(AxisLabel("joystick button 0"));
+		GUILayout.Label(AxisLabel("joystick button 1"));
+		GUILayout.Label(AxisLabel("joystick button 2"));
+		GUILayout.Label(AxisLabel("joystick button 3"));
+		GUILayout.Label(AxisLabel("joystick button 4"));
+		GUILayout.Label(AxisLabel("joystick button 5"));
+		GUILayout.Label(AxisLabel("joystick button 6"));
+		GUILayout.Label(AxisLabel("joystick button 7"));
+	}
+
+	string AxisLabel(string axisName) {
+		return axisName + ":" + Input.GetAxisRaw(axisName);
+	}
+
+	string ButtonLabel(string buttonName) {
+		return buttonName + ":" + Input.GetButton(buttonName);
+	}
+}
